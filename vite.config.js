e5cf10adb945fe537cf0149b5b7fa22d021a4f4c
Ashura2003 <shreejanm21@gmail.com>
2025-02-01 16:26:19 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import react from "@vitejs/plugin-react";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -14,9 +14,9 @@ export default defineConfig({
   },
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')),
+      key: fs.readFileSync(path.resolve(__dirname, "server.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "server.crt")),
     },
-    port: 5173, 
-  }
+    port: 5173,
+  },
 });
