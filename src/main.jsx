428070e8dd6cc6ee43ef7446821e242e14df5d8e
Ashura2003 <shreejanm21@gmail.com>
2025-02-02 13:36:07 +0545

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { AuthContextProvider } from './contexts/AuthContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App";
+import { AuthContextProvider } from "./contexts/AuthContext.jsx";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Use createRoot
+const root = ReactDOM.createRoot(document.getElementById("root")); // Use createRoot
 root.render(
-    <React.StrictMode>
-            <BrowserRouter>
-            <AuthContextProvider>
-                <ToastContainer theme='dark' position='top-right'
-                autoClose = {4000} closeOnClick pauseOnHover = {false} />
-                <App />
-            </AuthContextProvider>
-            </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <ToastContainer
+          position="top-right"
+          autoClose={4000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
